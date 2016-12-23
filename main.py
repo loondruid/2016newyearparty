f030d764a42db98fe1115d8eb516036c684aeb69
Druid <loondruid@gmail.com>
2016-12-23 13:33:08 +1000

    addition to main construction; separated cycles of render, update and event handling

--- main.py
+++ main.py
@@ -3,21 +3,60 @@ import os
 import time
 from pygame.locals import *
 
-def main():
-    HEIGHT = 500
-    WIDTH = 400
+class Game:
+    def __init__(self):
+        self.running = False
+        
+        self.width = 500
+        self.height = 500
+        
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.renderables = [] #renderable is something that has render() function
+        self.logicals = [] #logical is something that has update() function
+        self.eventhandlers = [] #has function handle(event from pygame.event.get())
+        
+        pygame.display.set_caption("Happy new year 2016! :D")
 
-    pygame.init()
-    screen = pygame.display.set_mode((HEIGHT, WIDTH))
-    pygame.display.set_caption("Happy new year 2016! :D")
+    
+    def run(self):
+        self.running = True
+        while (self.running):
+            self.logic()
+            self.render()
 
-    running = True
-    while (running):
+    
+    def logic(self):
         for event in pygame.event.get():
             if event.type == QUIT:
-                return
+                self.running = False
             elif event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
-                    return
+                    self.running = False
+            for eventhandler in self.eventhandlers:
+                eventhandler.handle(event)
+
+        for logical in self.logicals:
+            logical.update()
+
+    
+    def render(self):
+        self.screen.fill((100,254,110))
+        for renderable in self.renderables:
+            renderable.render()
+        pygame.display.flip();
+
+    
+    def set_height(self, height):
+        self.height = height
+
+    
+    def set_width(self, width):
+        self.width = width
+
+
+def main():
+    pygame.init()
+    game = Game()
+    game.run()
 
 main()
